ba21de4a2c74773aaedf98a5b56b8e469e083d33
Taddeus Kroes <taddeuskroes@gmail.com>
2013-07-26 17:39:47 +0200

    Refactored handshaking process, moved it to a separate file, implemented HTTP authentication

---
+++ handshake.py
@@ -0,0 +1,331 @@
+import os
+import re
+from hashlib import sha1
+from base64 import b64encode
+from urlparse import urlparse
+
+from python_digest import build_authorization_request
+from errors import HandshakeError
+
+
+WS_GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+WS_VERSION = '13'
+MAX_REDIRECTS = 10
+
+
+def split_stripped(value, delim=','):
+    return map(str.strip, str(value).split(delim)) if value else []
+
+
+class Handshake(object):
+    def __init__(self, wsock):
+        self.wsock = wsock
+        self.sock = wsock.sock
+
+    def fail(self, msg):
+        self.sock.close()
+        raise HandshakeError(msg)
+
+    def receive_request(self):
+        raw, headers = self.receive_headers()
+
+        # Request must be HTTP (at least 1.1) GET request, find the location
+        match = re.search(r'^GET (.*) HTTP/1.1\r\n', raw)
+
+        if match is None:
+            self.fail('not a valid HTTP 1.1 GET request')
+
+        location = match.group(1)
+        return location, headers
+
+    def receive_response(self):
+        raw, headers = self.receive_headers()
+
+        # Response must be HTTP (at least 1.1) with status 101
+        match = re.search(r'^HTTP/1\.1 (\d{3})', raw)
+
+        if match is None:
+            self.fail('not a valid HTTP 1.1 response')
+
+        status = int(match.group(1))
+        return status, headers
+
+    def receive_headers(self):
+        # Receive entire HTTP header
+        raw_headers = ''
+
+        while raw_headers[-4:] not in ('\r\n\r\n', '\n\n'):
+            raw_headers += self.sock.recv(512).decode('utf-8', 'ignore')
+
+        headers = {}
+
+        for key, value in re.findall(r'(.*?): ?(.*?)\r\n', raw_headers):
+            if key in headers:
+                headers[key] += ', ' + value
+            else:
+                headers[key] = value
+
+        return raw_headers, headers
+
+    def send_headers(self, headers):
+        # Send request
+        for hdr in list(headers):
+            if isinstance(hdr, tuple):
+                hdr = '%s: %s' % hdr
+
+            self.sock.sendall(hdr + '\r\n')
+
+        self.sock.sendall('\r\n')
+
+    def perform(self):
+        raise NotImplementedError
+
+
+class ServerHandshake(Handshake):
+    """
+    Executes a handshake as the server end point of the socket. If the HTTP
+    request headers sent by the client are invalid, a HandshakeError is raised.
+    """
+
+    def perform(self):
+        # Receive and validate client handshake
+        location, headers = self.receive_request()
+
+        # Send server handshake in response
+        self.send_headers(self.response_headers(location, headers))
+
+    def response_headers(self, location, headers):
+        # Check if headers that MUST be present are actually present
+        for name in ('Host', 'Upgrade', 'Connection', 'Sec-WebSocket-Key',
+                     'Sec-WebSocket-Version'):
+            if name not in headers:
+                self.fail('missing "%s" header' % name)
+
+        # Check WebSocket version used by client
+        version = headers['Sec-WebSocket-Version']
+
+        if version != WS_VERSION:
+            self.fail('WebSocket version %s requested (only %s is supported)'
+                      % (version, WS_VERSION))
+
+        # Verify required header keywords
+        if 'websocket' not in headers['Upgrade'].lower():
+            self.fail('"Upgrade" header must contain "websocket"')
+
+        if 'upgrade' not in headers['Connection'].lower():
+            self.fail('"Connection" header must contain "Upgrade"')
+
+        # Origin must be present if browser client, and must match the list of
+        # trusted origins
+        if 'Origin' not in headers:
+            if 'User-Agent' in headers:
+                self.fail('browser client must specify "Origin" header')
+
+            if self.wsock.trusted_origins:
+                self.fail('no "Origin" header specified, assuming untrusted')
+
+            origin = 'null'
+        elif self.wsock.trusted_origins:
+            origin = headers['Origin']
+
+            if origin not in self.wsock.trusted_origins:
+                self.fail('untrusted origin "%s"' % origin)
+
+        # Only a supported protocol can be returned
+        client_proto = split_stripped(headers['Sec-WebSocket-Protocol']) \
+                       if 'Sec-WebSocket-Protocol' in headers else []
+        protocol = 'null'
+
+        for p in client_proto:
+            if p in self.wsock.proto:
+                protocol = p
+                break
+
+        # Only supported extensions are returned
+        if 'Sec-WebSocket-Extensions' in headers:
+            client_ext = split_stripped(headers['Sec-WebSocket-Extensions'])
+            extensions = [e for e in client_ext if e in self.wsock.extensions]
+        else:
+            extensions = []
+
+        # Encode acceptation key using the WebSocket GUID
+        key = headers['Sec-WebSocket-Key'].strip()
+        accept = b64encode(sha1(key + WS_GUID).digest())
+
+        # Location scheme differs for SSL-enabled connections
+        scheme = 'wss' if self.wsock.secure else 'ws'
+
+        if 'Host' in headers:
+            host = headers['Host']
+        else:
+            host, port = self.sock.getpeername()
+            default_port = 443 if self.wsock.secure else 80
+
+            if port != default_port:
+                host += ':%d' % port
+
+        # Construct HTTP response header
+        yield 'HTTP/1.1 101 Web Socket Protocol Handshake'
+        yield 'Upgrade', 'websocket'
+        yield 'Connection', 'Upgrade'
+        yield 'WebSocket-Origin', origin
+        yield 'WebSocket-Location', '%s://%s%s' % (scheme, host, location)
+        yield 'Sec-WebSocket-Accept', accept
+        yield 'Sec-WebSocket-Protocol', protocol
+        yield 'Sec-WebSocket-Extensions', ', '.join(extensions)
+
+
+class ClientHandshake(Handshake):
+    """
+    Executes a handshake as the client end point of the socket. May raise a
+    HandshakeError if the server response is invalid.
+    """
+
+    def __init__(self, wsock):
+        Handshake.__init__(self, wsock)
+        self.redirects = 0
+
+    def perform(self):
+        self.send_headers(self.request_headers())
+        self.handle_response(*self.receive_response())
+
+    def handle_response(self, status, headers):
+        if status == 101:
+            self.handle_handshake(headers)
+        elif status == 401:
+            self.handle_auth(headers)
+        elif status in (301, 302, 303, 307, 308):
+            self.handle_redirect(headers)
+        else:
+            self.fail('invalid HTTP response status %d' % status)
+
+    def handle_handshake(self, headers):
+        # Check if headers that MUST be present are actually present
+        for name in ('Upgrade', 'Connection', 'Sec-WebSocket-Accept'):
+            if name not in headers:
+                self.fail('missing "%s" header' % name)
+
+        if 'websocket' not in headers['Upgrade'].lower():
+            self.fail('"Upgrade" header must contain "websocket"')
+
+        if 'upgrade' not in headers['Connection'].lower():
+            self.fail('"Connection" header must contain "Upgrade"')
+
+        # Verify accept header
+        accept = headers['Sec-WebSocket-Accept'].strip()
+        required_accept = b64encode(sha1(self.key + WS_GUID).digest())
+
+        if accept != required_accept:
+            self.fail('invalid websocket accept header "%s"' % accept)
+
+        # Compare extensions
+        if 'Sec-WebSocket-Extensions' in headers:
+            server_ext = split_stripped(headers['Sec-WebSocket-Extensions'])
+
+            for e in set(server_ext) - set(self.wsock.extensions):
+                self.fail('server extension "%s" unsupported by client' % e)
+
+            for e in set(self.wsock.extensions) - set(server_ext):
+                self.fail('client extension "%s" unsupported by server' % e)
+
+        # Assert that returned protocol (if any) is supported
+        if 'Sec-WebSocket-Protocol' in headers:
+            protocol = headers['Sec-WebSocket-Protocol']
+
+            if protocol != 'null' and protocol not in self.wsock.protocols:
+                self.fail('unsupported protocol "%s"' % protocol)
+
+            self.wsock.protocol = protocol
+
+
+    def handle_auth(self, headers):
+        # HTTP authentication is required in the request
+        hdr = headers['WWW-Authenticate']
+        authres = dict(re.findall(r'(\w+)[:=] ?"?(\w+)"?', hdr))
+        mode = hdr.lstrip().split(' ', 1)[0]
+
+        if not self.wsock.auth:
+            self.fail('missing username and password for HTTP authentication')
+
+        if mode == 'Basic':
+            auth_hdr = self.http_auth_basic_headers(**authres)
+        elif mode == 'Digest':
+            auth_hdr = self.http_auth_digest_headers(**authres)
+        else:
+            self.fail('unsupported HTTP authentication mode "%s"' % mode)
+
+        # Send new, authenticated handshake
+        self.send_headers(list(self.request_headers()) + list(auth_hdr))
+        self.handle_response(*self.receive_response())
+
+    def handle_redirect(self, headers):
+        self.redirects += 1
+
+        if self.redirects > MAX_REDIRECTS:
+            self.fail('reached maximum number of redirects (%d)'
+                      % MAX_REDIRECTS)
+
+        # Handle HTTP redirect
+        url = urlparse(headers['Location'].strip())
+
+        # Reconnect socket to new host if net location changed
+        if not url.port:
+            url.port = 443 if self.secure else 80
+
+        addr = (url.netloc, url.port)
+
+        if addr != self.sock.getpeername():
+            self.sock.close()
+            self.sock.connect(addr)
+
+        # Update websocket object and send new handshake
+        self.wsock.location = url.path
+        self.perform()
+
+    def request_headers(self):
+        if len(self.wsock.location) == 0:
+            self.fail('request location is empty')
+
+        # Generate a 16-byte random base64-encoded key for this connection
+        self.key = b64encode(os.urandom(16))
+
+        # Send client handshake
+        yield 'GET %s HTTP/1.1' % self.wsock.location
+        yield 'Host', '%s:%d' % self.sock.getpeername()
+        yield 'Upgrade', 'websocket'
+        yield 'Connection', 'keep-alive, Upgrade'
+        yield 'Sec-WebSocket-Key', self.key
+        yield 'Sec-WebSocket-Version', WS_VERSION
+
+        if self.wsock.origin:
+            yield 'Origin', self.wsock.origin
+
+        # These are for eagerly caching webservers
+        yield 'Pragma', 'no-cache'
+        yield 'Cache-Control', 'no-cache'
+
+        # Request protocols and extension, these are later checked with the
+        # actual supported values from the server's response
+        if self.wsock.protocols:
+            yield 'Sec-WebSocket-Protocol', ', '.join(self.wsock.protocols)
+
+        if self.wsock.extensions:
+            yield 'Sec-WebSocket-Extensions', ', '.join(self.wsock.extensions)
+
+    def http_auth_basic_headers(self, **kwargs):
+        u, p = self.wsock.auth
+        u = u.encode('utf-8')
+        p = p.encode('utf-8')
+        yield 'Authorization', 'Basic ' + b64encode(u + ':' + p)
+
+    def http_auth_digest_headers(self, **kwargs):
+        username, password = self.wsock.auth
+        yield 'Authorization', build_authorization_request(
+                                username=username.encode('utf-8'),
+                                method='GET',
+                                uri=self.wsock.location,
+                                nonce_count=0,
+                                realm=kwargs['realm'],
+                                nonce=kwargs['nonce'],
+                                opaque=kwargs['opaque'],
+                                password=password.encode('utf-8'))
--- websocket.py
+++ websocket.py
@@ -1,21 +1,9 @@
-import os
-import re
 import socket
 import ssl
-from hashlib import sha1
-from base64 import b64encode
-from urlparse import urlparse
 
 from frame import receive_frame
-from errors import HandshakeError, SSLError
-
-
-WS_GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-WS_VERSION = '13'
-
-
-def split_stripped(value, delim=','):
-    return map(str.strip, str(value).split(delim)) if value else []
+from handshake import ServerHandshake, ClientHandshake
+from errors import SSLError
 
 
 class websocket(object):
@@ -38,12 +26,13 @@ class websocket(object):
 
     Client example:
     >>> import twspy
-    >>> sock = twspy.websocket()
+    >>> sock = twspy.websocket(location='/my/path')
     >>> sock.connect(('', 8000))
     >>> sock.send(twspy.Frame(twspy.OPCODE_TEXT, 'Hello, Server!'))
     """
     def __init__(self, sock=None, protocols=[], extensions=[], origin=None,
-                 trusted_origins=[], sfamily=socket.AF_INET, sproto=0):
+                 trusted_origins=[], location='/', auth=None,
+                 sfamily=socket.AF_INET, sproto=0):
         """
         Create a regular TCP socket of family `family` and protocol
 
@@ -62,15 +51,23 @@ class websocket(object):
         has value not in this list, a HandshakeError is raised. If the list is
         empty (default), all origins are excepted.
 
+        `location` is optional, used for the HTTP handshake. In a URL, this
+        would show as ws://host[:port]/path.
+
+        `auth` is optional, used for HTTP Basic or Digest authentication during
+        the handshake. It must be specified as a (username, password) tuple.
+
         `sfamily` and `sproto` are used for the regular socket constructor.
         """
         self.protocols = protocols
         self.extensions = extensions
         self.origin = origin
         self.trusted_origins = trusted_origins
+        self.location = location
+        self.auth = auth
         self.sock = sock or socket.socket(sfamily, socket.SOCK_STREAM, sproto)
         self.secure = False
-        self.handshake_started = False
+        self.handshake_sent = False
 
     def bind(self, address):
         self.sock.bind(address)
@@ -87,24 +84,20 @@ class websocket(object):
         """
         sock, address = self.sock.accept()
         wsock = websocket(sock)
-        wsock.server_handshake()
+        ServerHandshake(wsock).perform()
+        wsock.handshake_sent = True
         return wsock, address
 
-    def connect(self, address, path='/', auth=None):
+    def connect(self, address):
         """
         Equivalent to socket.connect(), but sends an client handshake request
         after connecting.
 
         `address` is a (host, port) tuple of the server to connect to.
-
-        `path` is optional, used as the *location* part of the HTTP handshake.
-        In a URL, this would show as ws://host[:port]/path.
-
-        `auth` is optional, used for the HTTP "Authorization" header of the
-        handshake request.
         """
         self.sock.connect(address)
-        self.client_handshake(address, path, auth)
+        ClientHandshake(self).perform()
+        self.handshake_sent = True
 
     def send(self, *args):
         """
@@ -145,243 +138,13 @@ class websocket(object):
     def close(self):
         self.sock.close()
 
-    def server_handshake(self):
-        """
-        Execute a handshake as the server end point of the socket. If the HTTP
-        request headers sent by the client are invalid, a HandshakeError
-        is raised.
-        """
-        def fail(msg):
-            self.sock.close()
-            raise HandshakeError(msg)
-
-        # Receive HTTP header
-        raw_headers = ''
-
-        while raw_headers[-4:] not in ('\r\n\r\n', '\n\n'):
-            raw_headers += self.sock.recv(512).decode('utf-8', 'ignore')
-
-        # Request must be HTTP (at least 1.1) GET request, find the location
-        match = re.search(r'^GET (.*) HTTP/1.1\r\n', raw_headers)
-
-        if match is None:
-            fail('not a valid HTTP 1.1 GET request')
-
-        location = match.group(1)
-        headers = re.findall(r'(.*?): ?(.*?)\r\n', raw_headers)
-        header_names = [name for name, value in headers]
-
-        def header(name):
-            return ', '.join([v for n, v in headers if n == name])
-
-        # Check if headers that MUST be present are actually present
-        for name in ('Host', 'Upgrade', 'Connection', 'Sec-WebSocket-Key',
-                     'Sec-WebSocket-Version'):
-            if name not in header_names:
-                fail('missing "%s" header' % name)
-
-        # Check WebSocket version used by client
-        version = header('Sec-WebSocket-Version')
-
-        if version != WS_VERSION:
-            fail('WebSocket version %s requested (only %s '
-                                 'is supported)' % (version, WS_VERSION))
-
-        # Verify required header keywords
-        if 'websocket' not in header('Upgrade').lower():
-            fail('"Upgrade" header must contain "websocket"')
-
-        if 'upgrade' not in header('Connection').lower():
-            fail('"Connection" header must contain "Upgrade"')
-
-        # Origin must be present if browser client, and must match the list of
-        # trusted origins
-        if 'Origin' not in header_names:
-            if 'User-Agent' in header_names:
-                fail('browser client must specify "Origin" header')
-
-            if self.trusted_origins:
-                fail('no "Origin" header specified, assuming untrusted')
-        elif self.trusted_origins:
-            origin = header('Origin')
-
-            if origin not in self.trusted_origins:
-                fail('untrusted origin "%s"' % origin)
-
-        # Only supported protocols are returned
-        client_protocols = split_stripped(header('Sec-WebSocket-Extensions'))
-        protocol = 'null'
-
-        for p in client_protocols:
-            if p in self.protocols:
-                protocol = p
-                break
-
-        # Only supported extensions are returned
-        extensions = split_stripped(header('Sec-WebSocket-Extensions'))
-        extensions = [e for e in extensions if e in self.extensions]
-
-        # Encode acceptation key using the WebSocket GUID
-        key = header('Sec-WebSocket-Key').strip()
-        accept = b64encode(sha1(key + WS_GUID).digest())
-
-        # Construct HTTP response header
-        shake = 'HTTP/1.1 101 Web Socket Protocol Handshake\r\n'
-        shake += 'Upgrade: websocket\r\n'
-        shake += 'Connection: Upgrade\r\n'
-        shake += 'WebSocket-Origin: %s\r\n' % header('Origin')
-        shake += 'WebSocket-Location: ws://%s%s\r\n' \
-                 % (header('Host'), location)
-        shake += 'Sec-WebSocket-Accept: %s\r\n' % accept
-        shake += 'Sec-WebSocket-Protocol: %s\r\n' % protocol
-        shake += 'Sec-WebSocket-Extensions: %s\r\n' % ', '.join(extensions)
-
-        self.sock.sendall(shake + '\r\n')
-        self.handshake_started = True
-
-    def client_handshake(self, address, location, auth):
-        """
-        Executes a handshake as the client end point of the socket. May raise a
-        HandshakeError if the server response is invalid.
-        """
-        def fail(msg):
-            self.sock.close()
-            raise HandshakeError(msg)
-
-        def send_request(location):
-            if len(location) == 0:
-                fail('request location is empty')
-
-            # Generate a 16-byte random base64-encoded key for this connection
-            key = b64encode(os.urandom(16))
-
-            # Send client handshake
-            shake = 'GET %s HTTP/1.1\r\n' % location
-            shake += 'Host: %s:%d\r\n' % address
-            shake += 'Upgrade: websocket\r\n'
-            shake += 'Connection: keep-alive, Upgrade\r\n'
-            shake += 'Sec-WebSocket-Key: %s\r\n' % key
-            shake += 'Sec-WebSocket-Version: %s\r\n' % WS_VERSION
-
-            if self.origin:
-                shake += 'Origin: %s\r\n' % self.origin
-
-            # These are for eagerly caching webservers
-            shake += 'Pragma: no-cache\r\n'
-            shake += 'Cache-Control: no-cache\r\n'
-
-            # Request protocols and extension, these are later checked with the
-            # actual supported values from the server's response
-            if self.protocols:
-                shake += 'Sec-WebSocket-Protocol: %s\r\n' \
-                         % ', '.join(self.protocols)
-
-            if self.extensions:
-                shake += 'Sec-WebSocket-Extensions: %s\r\n' \
-                         % ', '.join(self.extensions)
-
-            if auth:
-                shake += 'Authorization: %s\r\n' % auth
-
-            self.sock.sendall(shake + '\r\n')
-            return key
-
-        def receive_response(key):
-            # Receive and process server handshake
-            raw_headers = ''
-
-            while raw_headers[-4:] not in ('\r\n\r\n', '\n\n'):
-                raw_headers += self.sock.recv(512).decode('utf-8', 'ignore')
-
-            # Response must be HTTP (at least 1.1) with status 101
-            match = re.search(r'^HTTP/1\.1 (\d{3})', raw_headers)
-
-            if match is None:
-                fail('not a valid HTTP 1.1 response')
-
-            status = int(match.group(1))
-            headers = re.findall(r'(.*?): ?(.*?)\r\n', raw_headers)
-            header_names = [name for name, value in headers]
-
-            def header(name):
-                return ', '.join([v for n, v in headers if n == name])
-
-            if status == 401:
-                # HTTP authentication is required in the request
-                raise HandshakeError('HTTP authentication required: %s'
-                                     % header('WWW-Authenticate'))
-
-            if status in (301, 302, 303, 307, 308):
-                # Handle HTTP redirect
-                url = urlparse(header('Location').strip())
-
-                # Reconnect socket if net location changed
-                if not url.port:
-                    url.port = 443 if self.secure else 80
-
-                addr = (url.netloc, url.port)
-
-                if addr != self.sock.getpeername():
-                    self.sock.close()
-                    self.sock.connect(addr)
-
-                # Send new handshake
-                receive_response(send_request(url.path))
-                return
-
-            if status != 101:
-                # 101 means server has accepted the connection and sent
-                # handshake headers
-                fail('invalid HTTP response status %d' % status)
-
-            # Check if headers that MUST be present are actually present
-            for name in ('Upgrade', 'Connection', 'Sec-WebSocket-Accept'):
-                if name not in header_names:
-                    fail('missing "%s" header' % name)
-
-            if 'websocket' not in header('Upgrade').lower():
-                fail('"Upgrade" header must contain "websocket"')
-
-            if 'upgrade' not in header('Connection').lower():
-                fail('"Connection" header must contain "Upgrade"')
-
-            # Verify accept header
-            accept = header('Sec-WebSocket-Accept').strip()
-            required_accept = b64encode(sha1(key + WS_GUID).digest())
-
-            if accept != required_accept:
-                fail('invalid websocket accept header "%s"' % accept)
-
-            # Compare extensions
-            server_ext = split_stripped(header('Sec-WebSocket-Extensions'))
-
-            for e in server_ext:
-                if e not in self.extensions:
-                    fail('server extension "%s" is unsupported by client' % e)
-
-            for e in self.extensions:
-                if e not in server_ext:
-                    fail('client extension "%s" is unsupported by server' % e)
-
-            # Assert that returned protocol (if any) is supported
-            protocol = header('Sec-WebSocket-Protocol')
-
-            if protocol:
-                if protocol != 'null' and protocol not in self.protocols:
-                    fail('unsupported protocol "%s"' % protocol)
-
-                self.protocol = protocol
-
-        self.handshake_started = True
-        receive_response(send_request(location))
-
     def enable_ssl(self, *args, **kwargs):
         """
         Transforms the regular socket.socket to an ssl.SSLSocket for secure
         connections. Any arguments are passed to ssl.wrap_socket:
         http://docs.python.org/dev/library/ssl.html#ssl.wrap_socket
         """
-        if self.handshake_started:
+        if self.handshake_sent:
             raise SSLError('can only enable SSL before handshake')
 
         self.secure = True
