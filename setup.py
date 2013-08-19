c1bcd852918db252ac1e82f049c9a78544be68b1
Taddeus Kroes <taddeuskroes@gmail.com>
2013-08-19 16:47:18 +0200

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='twspy',
+      version='0.8',
+      description='A standalone implementation of websockets (RFC 6455)',
+      author='Taddeus Kroes',
+      author_email='taddeuskroes@gmail.com',
+      url='https://github.com/taddeus/twspy',
+      package_dir={'twspy': '.'},
+      packages=['twspy'])
