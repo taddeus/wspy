553aa17f22908ada44dea02b9c03a72bc5177b5a
Taddeus Kroes <taddeuskroes@gmail.com>
2014-01-07 17:49:28 +0100

    Disabled SSL configuration in test server

--- test/server.py
+++ test/server.py
@@ -20,5 +20,5 @@ if __name__ == '__main__':
     deflate = WebkitDeflateFrame()
     #deflate = WebkitDeflateFrame(defaults={'no_context_takeover': True})
     EchoServer(('localhost', 8000), extensions=[deflate],
-               ssl_args=dict(keyfile='cert.pem', certfile='cert.pem'),
+               #ssl_args=dict(keyfile='cert.pem', certfile='cert.pem'),
                loglevel=logging.DEBUG).run()
