7fe869f4f442ddf4a5c8d607b002efbe7e898e98
Taddeus Kroes <taddeuskroes@gmail.com>
2014-12-21 21:07:46 +0100

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from distutils.core import setup
-
-
-setup(name='wspy',
-      version='0.9',
-      description='A standalone implementation of websockets (RFC 6455).',
-      author='Taddeus Kroes',
-      author_email='taddeuskroes@gmail.com',
-      url='https://github.com/taddeus/wspy',
-      package_dir={'wspy': '.'},
-      packages=['wspy'],
-      license='3-clause BSD License')
+setup(
+    name='wspy',
+    version='0.91',
+    description='A standalone implementation of websockets (RFC 6455).',
+    author='Taddeus Kroes',
+    author_email='taddeus@kompiler.org',
+    url='https://github.com/taddeus/wspy',
+    package_dir={'wspy': '.'},
+    packages=['wspy'],
+    license='3-clause BSD License'
+)
