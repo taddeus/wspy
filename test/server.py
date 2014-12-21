348b79cfd68afa91a71009a6481f2d45495909cf
Taddeus Kroes <taddeuskroes@gmail.com>
2014-12-21 22:11:16 +0100

    Test EchoServer now uses AsyncServer and deflate extensions

--- test/server.py
+++ test/server.py
@@ -6,19 +6,18 @@ from os.path import abspath, dirname
 basepath = abspath(dirname(abspath(__file__)) + '/..')
 sys.path.insert(0, basepath)
 
-from server import Server
+from async import AsyncServer
+from deflate_message import DeflateMessage
 from deflate_frame import DeflateFrame
 
 
-class EchoServer(Server):
+class EchoServer(AsyncServer):
     def onmessage(self, client, message):
-        Server.onmessage(self, client, message)
         client.send(message)
 
 
 if __name__ == '__main__':
     EchoServer(('localhost', 8000),
-               #extensions=[DeflateFrame(no_context_takeover=True)],
-               extensions=[DeflateFrame()],
+               extensions=[DeflateMessage(), DeflateFrame()],
                #ssl_args=dict(keyfile='cert.pem', certfile='cert.pem'),
                loglevel=logging.DEBUG).run()
